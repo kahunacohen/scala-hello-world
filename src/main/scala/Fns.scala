8d80e510da70971980d8b6051922d0f0094a6833
Aaron Cohen <acohen@mountainpasstech.com>
2020-09-14 20:57:57 +0300

    add comments

--- src/main/scala/Fns.scala
+++ src/main/scala/Fns.scala
@@ -13,10 +13,23 @@ object Fns {
     ret
   }
   
+  /*
+   * factorialRecurse(2)
+  - go(2, 1)
+  - n is not <= 0:
+    - go(2 - 1, 2*1)
+    - go (n=1,acc=2)
+  - n is not <= 0
+     - go(1-1, 1*2)
+     - go(n=0, acc=2)
+  - n  is <= 0:
+      - return acc=2
+  */
   def factorialRecurse(n: Int): Int = {
-    @tailrec def loop(x: Int): Int = {
-      if (x == 0) 1 else loop(x - 1)
+    @tailrec def go(n: Int, acc: Int): Int = {
+      if (n <= 0 ) acc
+      else go(n - 1, n*acc)
     }
-    loop(n)
+    go(n, 1)
   }
 }
